57bdcda5a3755a3f682f2d6dc127f11d2c92013f
StephenVinouze <s.vinouze@gmail.com>
2017-06-19 11:31:47 +0200

    Add workflow

---
+++ core/src/main/kotlin/com/wopata/register_core/preferences/UserPreferences.kt
@@ -0,0 +1,11 @@
+package com.wopata.register_core.extensions
+
+import android.content.Context
+import com.wopata.register_core.models.CustomUser
+
+/**
+ * Created by stephenvinouze on 14/06/2017.
+ */
+fun CustomUser.read(context: Context): CustomUser {
+
+}
